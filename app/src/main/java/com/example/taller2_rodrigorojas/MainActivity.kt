68d128c7c79bff17e3297ad0df2ab6d435e3f59f
Rodrivdlc <rodri.vdlc@gmail.com>
2024-10-07 20:09:46 +0200

    mainactivity

--- app/src/main/java/com/example/taller2_rodrigorojas/MainActivity.kt
+++ app/src/main/java/com/example/taller2_rodrigorojas/MainActivity.kt
@@ -3,45 +3,13 @@ package com.example.taller2_rodrigorojas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.taller2_rodrigorojas.ui.theme.Taller2_RodrigoRojasTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Taller2_RodrigoRojasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            // Llamar al sistema de navegación
+            AppNavigation()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Taller2_RodrigoRojasTheme {
-        Greeting("Android")
     }
 }
